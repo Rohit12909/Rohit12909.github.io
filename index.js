5f082fb8980b3849f1aa4e1d1f059d3516c40827
Rohit12909 <Rohit12909@gmail.com>
2023-09-20 16:46:15 -0400

    added classes to home page

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "250px";
   }
   
-  function closeNav() {
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
+}
+
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("class");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
+}
