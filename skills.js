0819d76240748bd536e0bd0def550425f3ef8eed
Rohit12909 <Rohit12909@gmail.com>
2023-09-20 14:56:58 -0400

    added offcanvas side bar and js files to all pages

--- skills.js
+++ skills.js
@@ -19,3 +19,13 @@ function openCity(evt, skill)
     document.getElementById(skill).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
