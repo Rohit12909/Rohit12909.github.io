21a51159803c136dca08ff7fa061867a06710481
Rohit12909 <Rohit12909@gmail.com>
2023-09-26 14:06:57 -0400

    centered skills.html, updated background all pages

--- projects.js
+++ projects.js
@@ -7,3 +7,14 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
   }
+
+  function randomColor() {
+    return '#' + ('000000' + (Math.random() * 0xFFFFFF << 0).toString(16)).slice(-6)
+  }
+  
+  function setColor() {
+    document.getElementById('backgrounds').style.backgroundColor = randomColor();
+    setTimeout(setColor, 2000);
+  }
+  
+  setColor();
